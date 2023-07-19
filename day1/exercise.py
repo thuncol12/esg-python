cadf8ae00610734024bf39ca97136a5b78a84f1f
김정례 <thuncol12@naver.com>
2023-07-19 16:31:15 +0900

    파이썬 set 실습

--- day1/exercise.py
+++ day1/exercise.py
@@ -41,3 +41,14 @@ a = {
     "D": 60
 }
 print(a["B"])
+
+# 교집합과 차집합
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+print(s1 & s2)
+print(s1-s2)
+
+# 중복이 없는 리스트로 출력하기
+my_list = [1, 2, 3, 4, 4, 4, 9, 11, 11, 11, 14]
+new_list = list(set(my_list))
+print(new_list)
