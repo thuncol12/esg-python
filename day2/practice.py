91b8143495a277fe4c580cfedecc7b24045a6dfb
김정례 <thuncol12@naver.com>
2023-07-20 11:29:35 +0900

    구구단 수정

--- day2/practice.py
+++ day2/practice.py
@@ -2,8 +2,8 @@
 # 구구단
 # 2-9단
 def gugudan():
-    for i in range(2, 10):
-        for j in range(1, 10):
+    for i in range(1, 10):
+        for j in range(2, 10):
             print(f'{j}*{i}={i*j}', end=' ')
         print()
 
